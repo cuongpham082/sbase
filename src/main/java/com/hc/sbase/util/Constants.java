6db1ce98ada3b4108a24f248262d9f3fce1ac2b2
CuongPham <pvcuong@tma.com.vn>
2021-03-15 15:41:00 +0700

    second comit

---
+++ src/main/java/com/hc/sbase/util/Constants.java
@@ -0,0 +1,13 @@
+package com.hc.sbase.util;
+
+public class Constants {
+
+	public static final String ACCESS_TOKEN_TYPE = "Bearer";
+	public static final String HEADER_AUTHORIZATION = "Authorization";
+	
+	public static final String LOGIN_URI = "/sbase/api/v1/login";
+	public static final String SIGNUP_URI = "/sbase/api/v1/signup";
+	public static final String TOKEN_URI = "/sbase/api/v1/token";
+	public static final String ACTIVE_USER_URI = "/sbase/api/v1/activeaccount";
+	public static final String FORGOT_PASSWORD_URI = "/sbase/api/v1/forgotpassword";
+}
---
+++ src/test/java/com/hc/sbase/SbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.sbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
